68ce274ad035a9429d2017d78421437606097c39
Mubashir Ahmed <imubasherahmad@gmail.com>
2022-08-14 03:14:05 +0500

    Time: 0 ms (100.00%), Space: 2.3 MB (87.70%) - LeetHub

---
+++ 14-longest-common-prefix/14-longest-common-prefix.go
@@ -0,0 +1,17 @@
+func longestCommonPrefix(strs []string) string {
+    lcp := len(strs[0])
+    for i := 1; i < len(strs); i++ {
+        count := 0
+        for j := 0; j < len(strs[i]) && j < len(strs[0]); j++ {
+            if strs[0][j] == strs[i][j] {
+                count++
+            } else {
+                break
+            }
+        }
+        if lcp > count {
+            lcp = count
+        }
+    }
+    return strs[0][:lcp]
+}
