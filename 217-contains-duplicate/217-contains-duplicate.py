093fe64521fb5f081efa8c639f523102f1992e70
Usama <uejaz.bese18seecs@seecs.edu.pk>
2022-08-14 01:40:57 +0400

    Time: 478 ms (91.26%), Space: 26.1 MB (5.20%) - LeetHub

---
+++ 217-contains-duplicate/217-contains-duplicate.py
@@ -0,0 +1,13 @@
+class Solution:
+    def containsDuplicate(self, nums: List[int]):
+        dic = {}
+        for i in nums:
+            if i in dic.keys():
+                dic[i] += 1
+                if dic[i] == 2:
+                    return True
+            else: 
+                dic[i] = 1
+        print(dic)
+        return False
+        
