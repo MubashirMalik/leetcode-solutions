3147ebae9ce601c0bf14339ddcaae4f17a06a3eb
Usama <uejaz.bese18seecs@seecs.edu.pk>
2022-08-15 02:23:51 +0400

    Time: 32 ms (95.65%), Space: 13.8 MB (63.35%) - LeetHub

--- 7-reverse-integer/7-reverse-integer.py
+++ 7-reverse-integer/7-reverse-integer.py
@@ -2,13 +2,11 @@ class Solution:
     def reverse(self, x: int) -> int:
         x = str(x)
         if x[0] == '-':
-            reversed_x = int(x[1:][::-1])
-            if reversed_x > 2**31:
-                return 0
-            return -reversed_x
+            reversed_x = -int(x[1:][::-1])
         else:
             reversed_x = int(x[::-1])
-            if reversed_x > 2**31 :
-                return 0
-            return int(x[::-1])
+            
+        if reversed_x > 2**31 or reversed_x < -2**31:
+            return 0
+        return reversed_x
         
