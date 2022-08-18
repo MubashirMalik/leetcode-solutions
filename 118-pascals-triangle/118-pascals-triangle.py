2461c0489f6f31109946b88253827cc588d8183b
Usama <uejaz.bese18seecs@seecs.edu.pk>
2022-08-19 01:38:01 +0400

    Time: 55 ms (29.18%), Space: 13.9 MB (17.88%) - LeetHub

---
+++ 118-pascals-triangle/118-pascals-triangle.py
@@ -0,0 +1,19 @@
+class Solution:
+    def generate(self, numRows: int) -> List[List[int]]:
+        
+        tri = []
+        for i in range(1, numRows + 1):
+            row = [1] * i
+            if i > 2:
+                prev = 0
+                s = 0
+                for j, k in enumerate(tri[-1]):
+        
+                    s = prev + k
+                    row[j] = s
+                    prev = k
+                    
+                    
+            tri.append(row)
+        
+        return tri
