8a7703acbcf7d56a0bc819eb9e1efe5c79724e08
Mubashir Ahmed <imubasherahmad@gmail.com>
2022-08-20 02:37:27 +0500

    Time: 15 ms (25.37%), Space: 2.8 MB (82.84%) - LeetHub

---
+++ 242-valid-anagram/242-valid-anagram.go
@@ -0,0 +1,24 @@
+func isAnagram(s string, t string) bool {
+    
+    if len(s) != len(t) {
+        return false
+    }
+    
+    m := make(map[byte]int)
+    for i := range s {
+        m[s[i]]++
+    }
+    
+    for i := range t {
+        if _, isFound := m[t[i]]; isFound {
+            if m[t[i]] == 1 {
+                delete(m, t[i])
+            } else {
+                m[t[i]]--   
+            }
+        } else {
+            return false
+        }
+    }
+    return true
+}
