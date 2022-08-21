32f0274c2b2d154a07b47c5c27cc8a5368c301b9
Mubashir Ahmed <imubasherahmad@gmail.com>
2022-08-21 06:04:55 +0500

    Time: 39 ms (14.74%), Space: 7 MB (26.32%) - LeetHub

---
+++ 501-find-mode-in-binary-search-tree/501-find-mode-in-binary-search-tree.go
@@ -0,0 +1,43 @@
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func findMode(root *TreeNode) []int {
+    if root.Left == nil && root.Right == nil {
+        return []int{root.Val}
+    }
+
+    occurrences := make(map[int]int)
+    
+    var InOrder func(root *TreeNode)
+    var max int 
+    
+    InOrder = func(root *TreeNode) {
+        if root == nil {
+            return
+        }
+        
+		occurrences[root.Val]++
+        if max < occurrences[root.Val] {
+            max = occurrences[root.Val]
+        }
+
+        InOrder(root.Left)
+        InOrder(root.Right)
+    }
+    InOrder(root)
+    
+    fmt.Println(occurrences)
+    var modes []int
+    for k,v := range occurrences {
+        if v == max {
+            modes = append(modes, k)
+        }
+    }
+    
+    return modes
+}
